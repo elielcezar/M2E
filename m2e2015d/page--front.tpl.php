d77d905e966cbab17614ab1b55298bf68700d87c
Eliel Cezar <elielcezar@gmail.com>
2015-02-05 18:32:05 -0200

    Tema M2E 2014

---
+++ m2e2015d/page--front.tpl.php
@@ -0,0 +1,193 @@
+<header> 
+  <div class="navbar navbar-default white animated" role="navigation">
+        <div class="container">
+          <div class="row">
+           <h1 class="m2e"><a href="<?php print $GLOBALS['base_path']; ?>">M2E Comunicação Digital</a></h1>            
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>            
+            </div>             
+            <div class="navbar-collapse collapse">
+              <ul class="links nav navbar-nav">
+                <li class="menu-home"><a href="<?php print $GLOBALS['base_path']; ?>">Home</a></li>
+                <!--li class="menu-servicos"><a href="<?php print $GLOBALS['base_path']; ?>servicos">O Que Fazemos</a></li-->
+                <li class="menu-portfolio"><a href="<?php print $GLOBALS['base_path']; ?>portfolio">Portfólio</a></li>            
+                <li class="menu-contato"><a href="<?php print $GLOBALS['base_path']; ?>contato">Contato</a></li>                    
+                <li class="menu-orcamento"><a href="<?php print $GLOBALS['base_path']; ?>orcamento">Solicite um Orçamento</a></li>                
+                <li class="menu-facebook"><a href="https://www.facebook.com/agenciam2e">Facebook</a></li>
+                <li class="menu-twitter"><a href="https://twitter.com/agenciam2e">Twitter</a></li>
+              </ul>           
+            </div><!--/.nav-collapse -->                 
+          </div>
+      </div>
+    </div>
+  
+<h1 class="m2e-home"><a href="<?php print $GLOBALS['base_path']; ?>">M2E Comunicação Digital</a></h1>            
+ 
+    
+    
+    <div class="destaque">
+      <div class="container">
+        <div class="row"> 
+          <div class="col-md-12">                       
+            <img src="<?php print $GLOBALS['base_path']; ?>sites/all/themes/m2e2015d/img/m2e_home.png" class="img-responsive" alt="Agencia M2E" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <p class="chamada">Desenvolvemos sites, logotipos, materiais impressos, realizamos gestão de mídias sociais e tudo o que a sua empresa precisar para se comunicar com seus clientes com qualidade e profissionalismo.</p>              
+          </div>
+        </div>
+        <div class="row">            
+            <div class="col-md-6"><a href="https://www.facebook.com/agenciam2e" class="fb">facebook.com/agenciam2e</a></div>
+            <div class="col-md-6"><a href="https://twitter.com/agenciam2e" class="twitter">twitter.com/agenciam2e</a></div>            
+        </div>             
+      </div><!-- destaque -->
+    </div>
+</header>    
+<!-- HEADER --> 
+    
+<div id="principal">
+<div class="container">
+
+
+       <div class="servicos-home">     
+         <div class="row">
+          <div class="col-sm-3 servico logo">
+              <div class="row">
+                <div class="ico"><img src="<?php print $GLOBALS['base_path']; ?>sites/all/themes/m2e2015d/img/ico_logo.png" class="img-responsive" alt="Logotipos" /></div>                
+                  <h5>Logotipos</h5>
+                  <p>Nós criamos logotipos e sistemas de identidade visual completos, garantindo a unidade visual de todos os materiais e produtos que estamparem a marca da sua empresa.</p>                
+              </div>  
+          </div>
+          <div class="col-sm-3 servico webdesign">
+              <div class="row">
+                <div class="ico"><img src="<?php print $GLOBALS['base_path']; ?>sites/all/themes/m2e2015d/img/ico_web.png" class="img-responsive" alt="Web Design" /></div>               
+                  <h5>Web Design</h5>
+                  <p>Criação de sites com foco em usabilidade e facilidade de manutenção. Desenvolvemos Blogs, Sites Institucionais, Portfólios, E-commerce, e o que mais você precisar.</p>               
+              </div>  
+          </div>
+           <div class="col-sm-3 servico facebook">
+              <div class="row">
+                <div class="ico"><img src="<?php print $GLOBALS['base_path']; ?>sites/all/themes/m2e2015d/img/ico_facebook.png" class="img-responsive" alt="Facebook" /></div>                
+                  <h5>Facebook</h5>
+                  <p>Administramos sua página no Facebook gerando conteúdo, imagens e monitorando a interação com o público. Transforme sua página em seu principal canal de relacionamento com seus clientes!</p>                
+              </div>  
+          </div>
+           <div class="col-sm-3 servico google">
+              <div class="row">
+                <div class="ico"><img src="<?php print $GLOBALS['base_path']; ?>sites/all/themes/m2e2015d/img/ico_google.png" class="img-responsive" alt="Google" /></div>                
+                  <h5>Anúncios no Google</h5>
+                  <p>Publique anúncios de sua empresa no Google! Eles são exibidos apenas quando alguém procura pelas palavras-chave que você escolheu e você só paga quando alguém clicar em seu anúncio.</p>                
+              </div>  
+          </div>
+        </div>
+      </div>
+     
+          <div id="carrossel" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#carrossel" data-slide-to="0" class="active"></li>
+            <li data-target="#carrossel" data-slide-to="1"></li>
+            <li data-target="#carrossel" data-slide-to="2"></li>
+            <li data-target="#carrossel" data-slide-to="3"></li>
+          </ol>
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner">
+            <div class="item active">
+              <a href="http://agenciam2e.com.br/portfolio/tribaltech-2014"><img src="<?php print drupal_get_path('theme', 'm2e2015d'); ?>/img/banner_tt.jpg" class="img-responsive" alt="Tribaltech" /></a>
+            </div>
+            <div class="item">
+               <a href="http://agenciam2e.com.br/portfolio/psicodelia"><img src="<?php print drupal_get_path('theme', 'm2e2015d'); ?>/img/banner_psicodelia.jpg" class="img-responsive" alt="Psicodelia" /></a>
+            </div>
+            <div class="item">
+               <a href="http://agenciam2e.com.br/portfolio/restaurante-girassol"><img src="<?php print drupal_get_path('theme', 'm2e2015d'); ?>/img/banner_girassol.jpg" class="img-responsive" alt="Girassol" /></a>
+            </div>
+            <div class="item">
+               <a href="http://agenciam2e.com.br/portfolio/uzlet"><img src="<?php print drupal_get_path('theme', 'm2e2015d'); ?>/img/banner_uzlet.jpg" class="img-responsive" alt="Uzlet" /></a>
+            </div>
+           
+          </div>
+          <!-- Controls -->
+          <a class="left carousel-control" href="#carrossel" role="button" data-slide="prev">Anterior</a>
+          <a class="right carousel-control" href="#carrossel" role="button" data-slide="next">Próximo</a>
+        </div>
+
+
+        <div class="destaque-2">
+            <div class="row">                
+                <div class="col-sm-6 bloco portfolio">                    
+                    <div class="col-sm-12">
+                        <h4>Conheça nosso Trabalho</h4>
+                        <p>Entre nossos clientes temos restaurantes, academias, escolas, escritórios e até um resort. Veja o que temos feito para eles!</p>
+                        <a class="botao" href="http://agenciam2e.com.br/portfolio">Veja nosso Portfólio ></a>
+                    </div>                                     
+                </div>
+                <div class="col-sm-6 bloco orcamento">                    
+                    <div class="col-sm-12">
+                        <h4>Solicite um orçamento</h4>
+                        <p>Precisando de um site? Ou quem sabe renovar seu logotipo? Seja qual for sua necessidade, entre em contato e vamos conversar sobre o seu projeto!</p>
+                        <a class="botao" href="http://agenciam2e.com.br/orcamento">Orçamento ></a>
+                    </div>                                 
+                </div>
+            </div>
+          </div>
+       
+  </div>
+</div>
+  </div>
+<footer>
+  <div class="rodape1">
+      <div class="container">
+          <div class="row">
+            <div class="col-sm-12"><a  class="fale-conosco" href="http://agenciam2e.com.br/orcamento">Solicite um Orçamento ></a></div>
+          </div>
+          <div class="row">
+              <div class="col-sm-4"><div class="fone"><span>Telefone/Whatsapp <br /> 41 9963-1609</span></div></div>
+              <div class="col-sm-4"><div class="email"><span>contato@agenciam2e.com.br</span></div></div>
+              <div class="col-sm-4"><div class="endereco"><span>Rua Anna Gbur Barcik, 499 <br /> Curitiba - PR</span></div></div>
+          </div>
+      </div>
+  </div>
+  <div class="rodape2">
+      <div class="container">
+        <div class="col-xs-6 facebook"><div class="fb-like" data-href="https://www.facebook.com/agenciam2e" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div></div>
+        <div class="col-xs-6 m2e"><img src="<?php print $GLOBALS['base_path']; ?>sites/all/themes/m2e2015d/img/m2e-rodape.png" class="img-responsive"></div>
+      </div>
+  </div>
+</footer>
+    
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="<?php print $GLOBALS['base_path']; ?>sites/all/themes/m2e2015d/js/bootstrap.min.js"></script>
+<script src="<?php print $GLOBALS['base_path']; ?>sites/all/themes/m2e2015d/js/jquery.parallax-1.1.3.js" type="text/javascript" ></script>
+<script src="<?php print $GLOBALS['base_path']; ?>sites/all/themes/m2e2015d/js/geral.js"></script>
+<script src="<?php print $GLOBALS['base_path']; ?>sites/all/themes/m2e2015d/js/jquery.inview.js"></script>
+<script>
+jQuery(document).ready(function(){    
+  
+    $('header').parallax("50%", 0.3);  
+    var janela = $(window).width();
+    if(janela>767){
+          $('.navbar').css('opacity','0');
+          $(document).scroll(function () {
+              var y = $(this).scrollTop();        
+              if (y > 20) {
+                $('.navbar').css('opacity','1');          
+              } else {          
+                $('.navbar').css('opacity','0');          
+              }
+          }); 
+    }
+  
+})
+</script>
+ <!--[if !IE]><!-->
+<script src="<?php print $GLOBALS['base_path']; ?>sites/all/themes/m2e2015d/js/custom_inview.js"></script>
+<!--<![endif]--> 
---
+++ m2e2015d/template.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+
+/* altera a extrutura do menu */
+/*function m2e2014_links__system_main_menu($variables) {
+  $html = "<div>\n";
+  $html .= "  <ul class=\"links nav\" id=\"topnav\">\n";
+  foreach ($variables['links'] as $link) {
+    $html .= "<li>".l($link['title'], $link['href'], $link)."</li>";
+  }
+  $html .= "  </ul>\n";
+  $html .= "</div>\n";
+  return $html;
+}*/
+
+/*function m2e2014_links__system_main_menu($variables) {
+  $html = "<div>\n";
+  $html .= "  <ul id=\"your_id\">\n";  
+  foreach ($variables['links'] as $key => $link) {
+    $html .= "<li>".l($link['title'], $link['path'], $link)."</li>";
+  }
+  $html .= "  </ul>\n";
+  $html .= "</div>\n";
+
+  return $html;
+}*/
+
+/*function m2e2014_links__system_main_menu($variables) {  
+    $html = '<ul id="menu">'."\n";  
+    foreach ($variables['links']['system_main-menu'] as $key => $link) {  
+        if(is_array($link)){  
+            $html .= "<li>".l($link['#title'], $link['#href'], $link)."</li>";  
+        }                                                                                                                       
+    }  
+    $html .= "  </ul>\n";  
+    return $html;  
+}*/
+
+?>
